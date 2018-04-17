e3cc6d70a4e41fc1aa6550f1b1b1b188246fa347
Paolo Brasolin <paolo.brasolin@gmail.com>
2018-04-18 00:24:11 +0200

    fix pathname blunder

--- lib/mnogootex/cli/recombobulator.rb
+++ lib/mnogootex/cli/recombobulator.rb
@@ -19,21 +19,21 @@ module Mnogootex
       def self.try_args(*args)
         main = Pathname.new(args.last || '')
         return [args, nil, nil] unless main.file?
-        [args[0..-2], main.realpath, default_cfg.load(main.realdirpath)]
+        [args[0..-2], main.realpath, default_cfg.load(main.dirname)]
       end
 
       def self.try_link(*args)
         link = Pathname.pwd.ascend.map { |p| p.join('.mnogootex.src') }.detect(&:symlink?)
         return [args, nil, nil] if link.nil?
         main = link.readlink
-        [args, main.realpath, default_cfg.load(main.realdirpath)]
+        [args, main.realpath, default_cfg.load(main.dirname)]
       end
 
       def self.try_cfgs(*args)
         yaml = Pathname.pwd.ascend.map { |p| p.join('.mnogootex.yml') }.detect(&:file?)
         return [args, nil, nil] if yaml.nil?
-        cfg = default_cfg.load(yaml.realdirpath)
-        main = yaml.realdirpath.join(cfg.fetch('main', ''))
+        cfg = default_cfg.load(yaml.dirname)
+        main = yaml.dirname.join(cfg.fetch('main', ''))
         [args, (main.file? ? main.realpath : nil), cfg]
       end
 
