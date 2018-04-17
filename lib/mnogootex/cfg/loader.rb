4ff93bd7b2dfac97314e25674ee6c796f4c245e6
Paolo Brasolin <paolo.brasolin@gmail.com>
2018-04-17 23:27:08 +0200

    refactor loader/recombobulator

--- lib/mnogootex/cfg/loader.rb
+++ lib/mnogootex/cfg/loader.rb
@@ -3,38 +3,23 @@
 require 'yaml'
 require 'pathname'
 
+require 'mnogootex/utils'
+
 module Mnogootex
   module Cfg
     class Loader < Hash
       def initialize(basename:, defaults:)
-        @paths = []
         @basename = basename
         merge! defaults
       end
 
       def load(pathname)
-        scan_cfg pathname.realpath
-        filter_cfg
-        merge! load_cfg
-      end
-
-      private
-
-      def scan_cfg(pathname)
-        @paths << pathname.join(@basename)
-        return if pathname.root?
-        scan_cfg pathname.parent
-      end
-
-      def filter_cfg
-        @paths.select!(&:readable?)
-        @paths.reject!(&:zero?)
-      end
-
-      def load_cfg
-        @paths.reverse!
-        @paths.map! { |pathname| YAML.load_file pathname }
-        @paths.inject(&:merge!) # TODO: deep merge
+        paths = pathname.realpath.descend.
+                map { |path| path.join(@basename) }.
+                select(&:exist?).reject(&:zero?)
+        configs = paths.map { |path| YAML.load_file path }
+        configs.each { |cfg| merge! cfg }
+        self
       end
     end
   end
--- lib/mnogootex/cli/recombobulator.rb
+++ lib/mnogootex/cli/recombobulator.rb
@@ -4,45 +4,42 @@
 
 require 'mnogootex/cfg'
 require 'mnogootex/cfg/loader'
+require 'mnogootex/utils'
 
 module Mnogootex
   module CLI
     module Recombobulator
-      class Error < StandardError; end
-      class DiscombobulatedError < Error; end
-
-      def self.parse_jobs_main(*args)
-        return [[], nil] if args.empty?
-        return [args[0..-2], args.last] if Pathname.new(args.last).file?
-        [args, nil]
-      end
-
       def self.parse(*args)
-        jobs, mainable = parse_jobs_main(*args)
-        cfg = Mnogootex::Cfg::Loader.new basename: Mnogootex::Cfg::BASENAME,
-                                         defaults: Mnogootex::Cfg::DEFAULTS
-
-        if !mainable.nil? && (main = Pathname.new(mainable)).file?
-          main = main.realpath
-          cfg.load main.dirname
-        elsif (main = Pathname.new('.mnogootex.src')).symlink?
-          main = main.readlink.realpath
-          cfg.load main.dirname
-        else
-          cfg.load Pathname.pwd
-          main = recombobulate_pwd cfg['main']
-        end
-
+        jobs, main, cfg = try_args(*args)
+        jobs, main, cfg = try_link(*args) if main.nil?
+        jobs, main, cfg = try_cfgs(*args) if main.nil?
         [jobs, main, cfg]
       end
 
-      def self.recombobulate_pwd(mainable)
-        raise DiscombobulatedError if mainable.nil?
-        main = Pathname.new(mainable)
-        raise DiscombobulatedError unless main.file?
-        main = main.realpath
-        raise DiscombobulatedError unless Pathname.pwd == main.dirname
-        main
+      def self.try_args(*args)
+        main = Pathname.new(args.last)
+        return [args, nil, nil] unless main.file?
+        [args[0..-2], main.realpath, default_cfg.load(main.realdirpath)]
+      end
+
+      def self.try_link(*args)
+        link = Pathname.pwd.ascend.map { |p| p.join('.mnogootex.src') }.detect(&:symlink?)
+        return [args, nil, nil] if link.nil?
+        main = link.readlink
+        [args, main.realpath, default_cfg.load(main.realdirpath)]
+      end
+
+      def self.try_cfgs(*args)
+        yaml = Pathname.pwd.ascend.map { |p| p.join('.mnogootex.yml') }.detect(&:file?)
+        return [args, nil, nil] if yaml.nil?
+        cfg = default_cfg.load(yaml.realdirpath)
+        main = yaml.realdirpath.join(cfg.fetch('main', ''))
+        [args, (main.file? ? main.realpath : nil), cfg]
+      end
+
+      def self.default_cfg
+        Mnogootex::Cfg::Loader.new basename: Mnogootex::Cfg::BASENAME,
+                                   defaults: Mnogootex::Cfg::DEFAULTS
       end
     end
   end
